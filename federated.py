d54f1f34ccfd98999e597ba8132fd38e16b30338
flipflopapp <navalnovel@gmail.com>
2019-07-21 16:45:19 +0000

    federated ai fixes

--- federated.py
+++ federated.py
@@ -1,12 +1,12 @@
-'''
-1. Load the entire data
-2. Convert it into hdf5 format
-3. write to file.
-4. call federated learning routine
-'''
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#@test {"output": "ignore"}
 
 from __future__ import absolute_import, division, print_function
-
 
 import collections
 from six.moves import range
@@ -21,10 +21,12 @@ import tempfile
 import h5py
 import six
 
-
 from configparser import ConfigParser
 import data_loader as dl
-default = True
+
+
+# In[2]:
+
 
 '''
 1. Put the images in data/images
@@ -54,10 +56,21 @@ client_list = cp["FEDERATED"].get("client_list").split(",")
 shuffle_buffer = cp["FEDERATED"].getint("shuffle_buffer")
 num_clients = cp["FEDERATED"].getint("number_clients")
 
-'''
-client_id: dataset<type_a>
-type_a: OrderedDict([('label', <tf.Tensor 'arg0:0' shape=(6) dtype=int8>), ('pixels', <tf.Tensor 'arg0:0' shape=(28,28) dtype=float32>)])
-'''
+output_dir, class_names, base_model_name, image_source_dir, csv_dir, batch_size, epochs, output_weights_name, model_weights_path, initial_learning_rate, client_list, shuffle_buffer, num_clients
+
+
+# In[3]:
+
+
+np.random.seed(0)
+
+nest = tf.contrib.framework.nest
+tf.compat.v1.enable_v2_behavior()
+
+
+# In[4]:
+
+
 def load_data():
     output = {}
     for client_item in client_list:
@@ -82,102 +95,95 @@ def create_fake_hdf5(arg_data):
                 user_group.create_dataset(name, data=values)
     return filepath
 
-
 def preprocess(dataset):
-
     def element_fn(element):
-        if default:
-            _val = collections.OrderedDict([
-                ('x', tf.reshape(element['pixels'], [-1])),
-                ('y', tf.reshape(element['label'], [-1])),
-            ])
-        else:
-            _val = collections.OrderedDict([
-                ('x', tf.reshape(element['pixels'], element['pixels'].shape)),
-                ('y', tf.reshape(element['label'], element['label'].shape)),
-            ])
-        return _val
+        return collections.OrderedDict([
+            ('x', tf.reshape(element['pixels'], element['pixels'].shape)),
+            ('y', tf.reshape(element['label'], element['label'].shape)),
+        ])
 
     return dataset.repeat(epochs).map(element_fn).shuffle(
         shuffle_buffer).batch(batch_size)
-#    return dataset.repeat(epochs).shuffle(shuffle_buffer).batch(batch_size)
+
+
+# In[5]:
+
+
+client_data_train = hdf5_client_data.HDF5ClientData( create_fake_hdf5(load_data()) )
+example_dataset = client_data_train.create_tf_dataset_for_client(
+    client_data_train.client_ids[0]
+)
+preprocessed_example_dataset = preprocess(example_dataset)
+sample_batch = nest.map_structure(
+    lambda x: x.numpy(), iter(preprocessed_example_dataset).next()
+)
+
+
+# In[6]:
+
+
+len(sample_batch['y'][0]), sample_batch['y']
+
+
+# In[7]:
+
 
 def make_federated_data(client_data, client_ids):
     return [preprocess(client_data.create_tf_dataset_for_client(x))
             for x in client_ids]
 
+sample_clients = client_data_train.client_ids[0:num_clients]
+federated_train_data = make_federated_data(client_data_train, sample_clients)
+
+
+# In[ ]:
+
+
+class MyBinaryCrossentropy(tf.keras.metrics.BinaryCrossentropy):
+     def __init__(self, from_logits=False, label_smoothing=0, name='accuracy', dtype=None):
+           super(MyBinaryCrossentropy, self).__init__(name, dtype=dtype)
+
+     def update_state(self, y_true, y_pred, sample_weight=None):
+           return super(MyBinaryCrossentropy, self).update_state(
+                   y_true,
+                   y_pred
+               )
+
+def loss_fn(y_true, y_pred):
+    return tf.reduce_mean(tf.keras.losses.binary_crossentropy(
+        y_true, y_pred))
+
 def create_compiled_keras_model():
     model = tf.keras.applications.densenet.DenseNet121(include_top=True, input_tensor=None, input_shape=None, pooling="avg", weights=model_weights_path, classes=7)
 #    optimizer = Adam(lr=initial_learning_rate)
     optimizer = gradient_descent.SGD(learning_rate=0.02)
-    def loss_fn(y_true, y_pred):
-        return tf.reduce_mean(tf.keras.losses.binary_crossentropy(
-            y_true, y_pred))
-    
-    def metric_fn(y_true, y_pred):
-        return tf.keras.metrics.binary_crossentropy(
-            y_true,
-            y_pred
-        )
     
     model.compile(
         loss = loss_fn,
         optimizer=optimizer,
-        metrics=['accuracy']
-#        metrics=[metric_fn]
+        metrics=[MyBinaryCrossentropy()]
     )
-    return model
-  
-def create_compiled_keras_model_default():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(
-          10, 
-          activation=tf.nn.softmax, 
-          kernel_initializer='zeros', 
-          input_shape=(28,28,)
-        )])
-    
-    def loss_fn(y_true, y_pred):
-        return tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(
-            y_true, y_pred))
-    model.compile(
-        loss=loss_fn,
-        optimizer=gradient_descent.SGD(learning_rate=0.02),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
     return model
 
 def model_fn():
-    if default:
-        keras_model = create_compiled_keras_model_default()
-    else:
-        keras_model = create_compiled_keras_model()
-    global sample_batch
+    keras_model = create_compiled_keras_model()
     return tff.learning.from_compiled_keras_model(keras_model, sample_batch)
 
-'''
-1. create hdf5 client data object.
-2. call the federated learning routine
-3. 
-'''
-if __name__ == "__main__":
-    nest = tf.contrib.framework.nest
-    tf.compat.v1.enable_v2_behavior()
-    if default:
-        client_data_train, client_data_test = tff.simulation.datasets.emnist.load_data()
-    else:
-        client_data_train = hdf5_client_data.HDF5ClientData(create_fake_hdf5(load_data()))
-    example_dataset = client_data_train.create_tf_dataset_for_client(
-    client_data_train.client_ids[0])
-    preprocessed_example_dataset = preprocess(example_dataset)
-#    preprocessed_example_dataset = example_dataset
-    global sample_batch
-    sample_batch = nest.map_structure(
-        lambda x: x.numpy(), iter(preprocessed_example_dataset).next())
-    sample_clients = client_data_train.client_ids[0:num_clients]
-    federated_train_data = make_federated_data(client_data_train, sample_clients)
-    iterative_process = tff.learning.build_federated_averaging_process(model_fn)
-    state = iterative_process.initialize()
-    for round_num in range(2, 11):
-        state, metrics = iterative_process.next(state, federated_train_data)
-        print('round {:2d}, metrics={}'.format(round_num, metrics))
-    print('done!')
+
+# In[ ]:
+
+
+iterative_process = tff.learning.build_federated_averaging_process(model_fn)
+
+print("done")
+
+state = iterative_process.initialize()
+for round_num in range(1, 11):
+    state, metrics = iterative_process.next(state, federated_train_data)
+    print('round {:2d}, metrics={}'.format(round_num, metrics))
+print('done!')
+# In[ ]:
+
+
+
+
