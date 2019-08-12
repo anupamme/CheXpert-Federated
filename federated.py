5f19a618663b7238e6ac87d64e5f88179250d7bb
Anupam Mediratta <mediratta@gmail.com>
2019-08-12 11:39:34 -0400

    cleaning code

--- federated.py
+++ federated.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-#@test {"output": "ignore"}
-
 from __future__ import absolute_import, division, print_function
 
 import collections
@@ -58,18 +53,10 @@ num_clients = cp["FEDERATED"].getint("number_clients")
 
 output_dir, class_names, base_model_name, image_source_dir, csv_dir, batch_size, epochs, output_weights_name, model_weights_path, initial_learning_rate, client_list, shuffle_buffer, num_clients
 
-
-# In[3]:
-
-
 np.random.seed(0)
 
 nest = tf.contrib.framework.nest
 tf.compat.v1.enable_v2_behavior()
-
-
-# In[4]:
-
 
 def load_data():
     output = {}
@@ -105,10 +92,6 @@ def preprocess(dataset):
     return dataset.repeat(epochs).map(element_fn).shuffle(
         shuffle_buffer).batch(batch_size)
 
-
-# In[5]:
-
-
 client_data_train = hdf5_client_data.HDF5ClientData( create_fake_hdf5(load_data()) )
 example_dataset = client_data_train.create_tf_dataset_for_client(
     client_data_train.client_ids[0]
@@ -118,15 +101,7 @@ sample_batch = nest.map_structure(
     lambda x: x.numpy(), iter(preprocessed_example_dataset).next()
 )
 
-
-# In[6]:
-
-
 len(sample_batch['y'][0]), sample_batch['y']
-
-
-# In[7]:
-
 
 def make_federated_data(client_data, client_ids):
     return [preprocess(client_data.create_tf_dataset_for_client(x))
@@ -135,19 +110,15 @@ def make_federated_data(client_data, client_ids):
 sample_clients = client_data_train.client_ids[0:num_clients]
 federated_train_data = make_federated_data(client_data_train, sample_clients)
 
-
-# In[ ]:
-
-
 class MyBinaryCrossentropy(tf.keras.metrics.BinaryCrossentropy):
-     def __init__(self, from_logits=False, label_smoothing=0, name='accuracy', dtype=None):
-           super(MyBinaryCrossentropy, self).__init__(name, dtype=dtype)
+    def __init__(self, from_logits=False, label_smoothing=0, name='accuracy', dtype=None):
+        super(MyBinaryCrossentropy, self).__init__(name, dtype=dtype)
 
-     def update_state(self, y_true, y_pred, sample_weight=None):
-           return super(MyBinaryCrossentropy, self).update_state(
-                   y_true,
-                   y_pred
-               )
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        return super(MyBinaryCrossentropy, self).update_state(
+            y_true,
+            y_pred
+        )
 
 def loss_fn(y_true, y_pred):
     return tf.reduce_mean(tf.keras.losses.binary_crossentropy(
@@ -157,11 +128,11 @@ def create_compiled_keras_model():
     model = tf.keras.applications.densenet.DenseNet121(include_top=True, input_tensor=None, input_shape=None, pooling="avg", weights=model_weights_path, classes=7)
 #    optimizer = Adam(lr=initial_learning_rate)
     optimizer = gradient_descent.SGD(learning_rate=0.02)
-    
     model.compile(
         loss = loss_fn,
         optimizer=optimizer,
-        metrics=[MyBinaryCrossentropy()]
+#        metrics=[MyBinaryCrossentropy()]
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
     )
     return model
 
@@ -169,21 +140,8 @@ def model_fn():
     keras_model = create_compiled_keras_model()
     return tff.learning.from_compiled_keras_model(keras_model, sample_batch)
 
-
-# In[ ]:
-
-
 iterative_process = tff.learning.build_federated_averaging_process(model_fn)
-
-print("done")
-
 state = iterative_process.initialize()
 for round_num in range(1, 11):
     state, metrics = iterative_process.next(state, federated_train_data)
     print('round {:2d}, metrics={}'.format(round_num, metrics))
-print('done!')
-# In[ ]:
-
-
-
-
